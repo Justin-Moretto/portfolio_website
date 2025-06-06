1a91c7912ad0cf6851ee14fc156e0477debcf1c9
Justin <justinmorettomtl@gmail.com>
2025-06-05 22:25:17 -0400

    updated summary

--- src/Summary.jsx
+++ src/Summary.jsx
@@ -2,16 +2,12 @@ function Summary(params) {
   return(
     <section>
       <p id="summary">
-        I am an enthusiastic young developer driven by a passion for continuous learning and growth.
-        Three years ago, I made a career shift into tech and am now working with the talented people at
-        Runes Technologies Inc. Much of my professional experience thus far has been in working for consultancies
-        which has honed my ability to adapt to working on a variety of projects alongside many different developers.
-        I have a keen eye for quality and a deep commitment to communication and authenticity in every project.
-        <br />
-        <br/>
-        Currently, I am seeking new opportunities to further develop my skills
-        and contribute my expertise to a forward-thinking team. If you are looking for a dedicated,
-        quick learner who values both personal and professional growth, I would be an excellent addition to your organization!
+      Motivated and versatile software developer with five years of professional experience in mobile and web development, and years prior to that of working in QA while coding as a hobby.
+      Passionate about building scalable, high-quality applications and following exciting developments in tech, such as AI.
+      <br></br><br></br>
+      Proven track record in Agile Scrum environments, working closely with cross-functional teams. Continuously improving skills, including regularly participating in the FlutterMTL community and developing personal projects.
+      <br></br><br></br>
+      Currently seeking a new opportunity within a growth- and innovation-focused organization.
       </p>
     </section>
   )
