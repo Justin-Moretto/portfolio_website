c9e3ce4a51d8e3dd24acb47b217fb1d1ceec77b1
Justin <justinmorettomtl@gmail.com>
2025-06-15 02:16:45 -0400

    added CVs and CV links

--- src/Links.jsx
+++ src/Links.jsx
@@ -1,51 +1,30 @@
 function Links(params) {
   return (
     <div id='header-links'>
-      {/* TODO: Add CV download links after importing PDFs */}
-      {/* <a className="link" href="./docs/Resume_EN.pdf" target="_blank" rel="noopener noreferrer">CV (EN)</a> */}
-      {/* <a className="link" href="./docs/Resume_FR.pdf" target="_blank" rel="noopener noreferrer">CV (FR)</a> */}
       <a className="button-link header_link" href="https://github.com/Justin-Moretto" target="_blank" rel="noopener noreferrer">Github</a>
       <a className="button-link header_link" href="https://www.linkedin.com/in/justinmoretto/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
       <a className="button-link header_link" href="https://justinmoretto.itch.io/" target="_blank" rel="noopener noreferrer">Itch.io</a>
-      {/* <br></br>
+
       <div className="cv-links">
-        <div>
-          <a
-            href="/cv-justin-moretto-en.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="button-link CV-link"
-          >
-            CV (EN)
-          </a>
-          <a
-            href="/cv-justin-moretto-en.pdf"
-            download
-            className="button-link download-link"
-          >
-            V
-          </a>
-        </div>
-        <div>
-          <a
-            href="/cv-justin-moretto-fr.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="button-link CV-link"
-          >
-            CV (FR)
-          </a>
-          <a
-            href="/cv-justin-moretto-en.pdf"
-            download
-            className="button-link download-link"
-          >
-            V
-          </a>
-        </div>
-      </div> */}
+        <a
+          href="/JMoretto_CV_en.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="button-link cv-link"
+        >
+          CV (EN)
+        </a>
+        <a
+          href="/JMoretto_CV_fr.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="button-link cv-link"
+        >
+          CV (FR)
+        </a>
+      </div>
     </div>
   )
 }
 
-export default Links
+export default Links;
